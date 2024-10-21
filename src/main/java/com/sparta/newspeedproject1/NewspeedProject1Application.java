44dbe7c4d34eaa0d657e9b6d5b257b9ee67d2985
Yeon Woo CHO <102777664+yeonwoochocho@users.noreply.github.com>
2024-10-21 12:42:51 +0900

    branch test

---
+++ src/main/java/com/sparta/newspeedproject1/NewspeedProject1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.newspeedproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspeedProject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewspeedProject1Application.class, args);
+    }
+
+}
