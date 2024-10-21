69d1032e9fcf832a7e267fb54729dc0f0b93561f
hwk9407 <hwk__@naver.com>
2024-10-22 00:02:37 +0900

    [feat] 일정 생성 기능 추가

--- src/main/java/com/sparta/newspeed/board/service/BoardService.java
+++ src/main/java/com/sparta/newspeed/board/service/BoardService.java
@@ -1,4 +1,40 @@
 package com.sparta.newspeed.board.service;
 
+import com.sparta.newspeed.board.dto.CreateBoardRequestDto;
+import com.sparta.newspeed.board.dto.CreateBoardResponseDto;
+import com.sparta.newspeed.domain.board.Board;
+import com.sparta.newspeed.domain.board.BoardRepository;
+import com.sparta.newspeed.domain.user.User;
+import com.sparta.newspeed.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class BoardService {
+
+    private final BoardRepository boardRepository;
+//    private final UserRepository userRepository;
+
+
+    @Transactional
+    public CreateBoardResponseDto createBoard(CreateBoardRequestDto reqDto) {
+        // 임시 가짜 유저 엔티티 생성
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("하하하");
+        user.setPassword("1234");
+        user.setEmail("asdf@gmail.com");
+        // 임시
+
+        Board board = new Board(
+                user,
+                reqDto.getTitle(),
+                reqDto.getContent()
+        );
+        boardRepository.save(board);
+
+        return new CreateBoardResponseDto("201", "게시물 생성 완료", user.getId());
+    }
 }
