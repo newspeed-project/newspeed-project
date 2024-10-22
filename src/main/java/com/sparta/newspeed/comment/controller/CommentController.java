75c0c6234bbffb76ed4d107d68fba6f9572aaca7
lastdove <tpqms9917@gmail.com>
2024-10-23 00:18:50 +0900

    [댓글] * 댓글 삭제 기능 추가  - 댓글이 있는지 확인  - 댓글 작성자 또는 해당 게시글 작성자인지 확인 후 댓글 삭제

--- src/main/java/com/sparta/newspeed/comment/controller/CommentController.java
+++ src/main/java/com/sparta/newspeed/comment/controller/CommentController.java
@@ -2,7 +2,9 @@ package com.sparta.newspeed.comment.controller;
 
 import com.sparta.newspeed.comment.service.CommentService;
 import com.sparta.newspeed.comment.dto.ReadAllCommentResponseDto;
+import com.sparta.newspeed.domain.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,4 +22,11 @@ public class CommentController {
         ReadAllCommentResponseDto comments = commentService.getCommentsByBoardId(boardId);
         return ResponseEntity.ok(comments);
     }
+
+    @DeleteMapping("/comments/{id}")
+    public ResponseEntity<Void> deleteComment(@PathVariable Long boardId, @PathVariable Long id, @RequestAttribute("user") User jwtUser) {
+        commentService.deleteComment(boardId, id, jwtUser);
+        return ResponseEntity.noContent().build();
+    }
+
 }
