77fcfeae7a28ed61018224a3ef8e8dd655d7514c
hwk9407 <hwk__@naver.com>
2024-10-22 23:54:26 +0900

    [fix] 친구 API 충돌 수정

--- src/main/java/com/sparta/newspeed/friend/dto/FriendRequestDto.java
+++ src/main/java/com/sparta/newspeed/friend/dto/FriendRequestDto.java
@@ -1,10 +1,11 @@
 package com.sparta.newspeed.friend.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class FriendRequestDto {
-    @NotBlank
+    @NotNull
     private Long friendId;
 }
