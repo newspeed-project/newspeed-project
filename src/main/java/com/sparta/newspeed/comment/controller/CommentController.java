05929823c42f89e3d4d68b75a01770057d8d62fe
Yeon Woo CHO <102777664+yeonwoochocho@users.noreply.github.com>
2024-10-22 19:55:21 +0900

    댓글 수정 테스트

--- src/main/java/com/sparta/newspeed/comment/controller/CommentController.java
+++ src/main/java/com/sparta/newspeed/comment/controller/CommentController.java
@@ -6,19 +6,20 @@ import com.sparta.newspeed.domain.board.Board;
 import com.sparta.newspeed.domain.comment.Comment;
 import com.sparta.newspeed.comment.service.CommentService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("api/board/{boardId}")
 public class CommentController {
     private final CommentService commentService;
 
     // 의존성 주입 - 생성자 주입 방식 사용
-    public CommentController(CommentService commentService) {
-        this.commentService = commentService;
-    }
+//    public CommentController(CommentService commentService) {
+//        this.commentService = commentService;
+//    }
+
     // 1. 댓글 추가
     @PostMapping("/comments")
     public Comment saveComment(
@@ -34,7 +35,6 @@ public class CommentController {
     public Comment updateComment(
             @PathVariable Long commentId,
             @Valid @RequestBody CommentRequestDto requestDto) {
-
 
         return commentService.updateComment(commentId, requestDto);
     }
