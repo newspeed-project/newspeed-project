257ac38bc4a63068c233395e0acda0b29632f59d
lastdove <tpqms9917@gmail.com>
2024-10-23 21:39:47 +0900

    feat(feature/81) : 게시물&댓글 문제 수정  - 전체 게시물 조회 시 친구가 없는 경우 조회가 안되던 점 수정  - 댓글 삭제 시 게시글 작성자 또는 댓글 작성자가 아니여도 삭제가 되던 점 수정

--- src/main/java/com/sparta/newspeed/domain/board/BoardRepository.java
+++ src/main/java/com/sparta/newspeed/domain/board/BoardRepository.java
@@ -8,5 +8,5 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
     List<Board> findByUserIdIn(List<Long> userIds); // 친구 게시물 조회
     List<Board> findByUserIdNotIn(List<Long> userIds); // 비친구 게시물 조회
-
+    List<Board> findAllByOrderByModifiedAtDesc();
 }
