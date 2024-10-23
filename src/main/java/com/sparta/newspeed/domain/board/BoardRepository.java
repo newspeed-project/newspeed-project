aa3c091f8a95e3e647f3ca88809c5b7a319967fa
lastdove <tpqms9917@gmail.com>
2024-10-23 19:55:45 +0900

    feat(feature/70) : 친구 게시물 최상단 기능 추가  - 전체 게시물 조회 시 유저의 친구 게시물을 최상단으로 조회  - 전체 게시물 + 친구 게시물 모두 수정일자 기준 최신순으로 반영

--- src/main/java/com/sparta/newspeed/domain/board/BoardRepository.java
+++ src/main/java/com/sparta/newspeed/domain/board/BoardRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    List<Board> findAllById(Long id);
+
+    List<Board> findByUserIdIn(List<Long> userIds); // 친구 게시물 조회
+    List<Board> findByUserIdNotIn(List<Long> userIds); // 비친구 게시물 조회
+
 }
