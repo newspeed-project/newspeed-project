8f9a511b833e055d264ffee365b72d68f3691a33
Yeon Woo CHO <102777664+yeonwoochocho@users.noreply.github.com>
2024-10-23 10:49:49 +0900

    댓글 수정 - 수정 권한 구현 , 삭제 권한미구현

--- src/main/java/com/sparta/newspeed/comment/service/CommentService.java
+++ src/main/java/com/sparta/newspeed/comment/service/CommentService.java
@@ -1,36 +1,61 @@
 package com.sparta.newspeed.comment.service;
 
-import com.sparta.newspeed.domain.user.User;
+import com.sparta.newspeed.comment.dto.CommentRequestDto;
 import com.sparta.newspeed.comment.dto.ReadAllCommentResponseDto;
-import com.sparta.newspeed.domain.comment.Comment;
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import com.sparta.newspeed.domain.comment.CommentRepository;
 import com.sparta.newspeed.common.exception.ResourceNotFoundException;
-import org.springframework.web.bind.annotation.RequestAttribute;
+import com.sparta.newspeed.domain.board.Board;
+import com.sparta.newspeed.domain.board.BoardRepository;
+import com.sparta.newspeed.domain.comment.Comment;
+import com.sparta.newspeed.domain.comment.CommentRepository;
+import com.sparta.newspeed.domain.user.User;
+import com.sparta.newspeed.domain.user.UserRepository;
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final BoardRepository boardRepository;
+    private final UserRepository userRepository;
 
+    public CommentService(CommentRepository commentRepository, BoardRepository boardRepository, UserRepository userRepository) {
+        this.commentRepository = commentRepository;
+        this.boardRepository = boardRepository;
+        this.userRepository = userRepository;
+    }
+
+    //댓글 생성
+    @Transactional
+    public Comment saveComment(Long boardId, CommentRequestDto requestDto, User jwtUser) {
+        // 게시물 확인
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(() -> new ResourceNotFoundException("해당 게시물이 존재하지 않습니다. ID: " + boardId));
+
+        // 댓글 저장
+        Comment comment = new Comment();
+        comment.saveComment(requestDto.getContent(), board, jwtUser);
+
+        return commentRepository.save(comment);
+    }
+
+    //댓글 조회
     public ReadAllCommentResponseDto getCommentsByBoardId(Long boardId) {
         List<Comment> comments = commentRepository.findAllByBoardId(boardId);
         return new ReadAllCommentResponseDto("200", "특정 게시물의 댓글 조회 완료", comments);
     }
 
+    //댓글 수정
     @Transactional
-    public void deleteComment(Long boardId, Long id, User jwtUser) {
-            Comment comment = commentRepository.findById(id)
-                    .orElseThrow(() -> new ResourceNotFoundException("해당 댓글이 존재하지 않습니다."));
+    public Comment updateComment(Long commentId, CommentRequestDto requestDto, User jwtUser) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new ResourceNotFoundException("해당 댓글이 존재하지 않습니다. ID: " + commentId));
 
-            if (comment.getBoard().getId().equals(boardId) || jwtUser.getId().equals(comment.getUser().getId())) {
-                commentRepository.delete(comment);
-            }
+        comment.setContent(requestDto.getContent());
+
+        return commentRepository.save(comment);
     }
 }
