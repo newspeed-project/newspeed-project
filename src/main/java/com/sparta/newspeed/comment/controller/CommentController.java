dd5775069f70ebd4448a0080c7e901d0018af43b
hwk9407 <hwk__@naver.com>
2024-10-23 20:23:20 +0900

    [fix] 댓글 관련 API에 상태코드와 메시지가 출력되도록 변경

--- src/main/java/com/sparta/newspeed/comment/controller/CommentController.java
+++ src/main/java/com/sparta/newspeed/comment/controller/CommentController.java
@@ -1,14 +1,15 @@
 package com.sparta.newspeed.comment.controller;
 
+import com.sparta.newspeed.comment.dto.CommentOneResponseDto;
 import com.sparta.newspeed.comment.dto.CommentRequestDto;
 import com.sparta.newspeed.comment.dto.CommentResponseDto;
-import com.sparta.newspeed.comment.dto.ReadAllCommentResponseDto;
+import com.sparta.newspeed.comment.dto.CommentListResponseDto;
 
-import com.sparta.newspeed.domain.comment.Comment;
 import com.sparta.newspeed.comment.service.CommentService;
 import com.sparta.newspeed.domain.user.User;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,30 +23,30 @@ public class CommentController {
 
     // 1. 댓글 추가
     @PostMapping("/comments")
-    public ResponseEntity<CommentResponseDto> saveComment(
+    public ResponseEntity<CommentOneResponseDto> saveComment(
             @PathVariable Long boardId,
             @Valid @RequestBody CommentRequestDto requestDto,
             @RequestAttribute("user") User jwtUser) {
-        CommentResponseDto res = commentService.saveComment(boardId, requestDto, jwtUser);
-        return ResponseEntity.ok(res);
+        CommentOneResponseDto resDto = commentService.saveComment(boardId, requestDto, jwtUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(resDto);
     }
 
     //2. 댓글 조회
     @GetMapping("/comments")
-    public ResponseEntity<ReadAllCommentResponseDto> getComments(@PathVariable Long boardId) {
-        ReadAllCommentResponseDto comments = commentService.getCommentsByBoardId(boardId);
-        return ResponseEntity.ok(comments);
+    public ResponseEntity<CommentListResponseDto> getComments(@PathVariable Long boardId) {
+        CommentListResponseDto resDtos = commentService.getCommentsByBoardId(boardId);
+        return ResponseEntity.status(HttpStatus.OK).body(resDtos);
     }
 
     // 3. 댓글 수정
     @PutMapping("/comments/{id}")
-    public ResponseEntity<CommentResponseDto> updateComment(
+    public ResponseEntity<CommentOneResponseDto> updateComment(
             @PathVariable Long commentId,
             @Valid @RequestBody CommentRequestDto requestDto,
             @RequestAttribute("user") User jwtUser) {
 
-        CommentResponseDto res = commentService.updateComment(commentId, requestDto, jwtUser);
-        return ResponseEntity.ok(res);
+        CommentOneResponseDto resDto = commentService.updateComment(commentId, requestDto, jwtUser);
+        return ResponseEntity.status(HttpStatus.OK).body(resDto);
     }
 
     // 4. 댓글 삭제
