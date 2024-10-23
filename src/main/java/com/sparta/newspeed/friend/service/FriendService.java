452d74eb5d76873b4441a57d3beae72cdf046c00
yokxim <yooksi53@gmail.com>
2024-10-23 21:14:12 +0900

    [chore] FriendService 불필요한 import 삭제

--- src/main/java/com/sparta/newspeed/friend/service/FriendService.java
+++ src/main/java/com/sparta/newspeed/friend/service/FriendService.java
@@ -14,7 +14,6 @@ import com.sparta.newspeed.friend.dto.FriendRequestListResponseDto;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
